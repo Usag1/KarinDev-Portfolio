524bade780297815c216a5ce286acfaddeaa0d8a
Usag1 <karin_ikenaga@protonmail.com>
2021-02-17 01:59:22 +0000

    About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,21 @@
   <div class="about">
     <h2>About me</h2>
     <h3>Hello World<br>I'm Karin</h3>
-    <p>I am a self-taught programmer.
-       
-      </p>
+    <p>I am a self-taught programmer.<br>
+      I started studying programming in 2019,<br>and Create interfaces with HTML, CSS, Javascript, React, Vue and Node.<br>
+      Not only active in web design and frontend,<br>but also studying backend fields such as API and SQL.<br>
+      Since 2018, I have been living between Denmark and Japan.<br>
+      Currently, my life is mainly in Denmark .<br>
+      I am looking forward to collaborating and working with you. 
+    </p>
+    <img src="../assets/Frontend.jpg" alt="portfolio1">
   </div>
 </template>
 
+<style scoped>
+.about p {
+  font-size: 1rem;
+  line-height: 19px;
+}
+</style>
 
